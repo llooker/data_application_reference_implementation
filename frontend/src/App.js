0aaf4243ad7cd2bd46092737b1048bf736d4221e
Tom Pakeman <tegpakeman@gmail>
2020-10-20 11:56:48 +0100

    Moved select example to main index page and tidied up routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,70 @@
-import * as React from 'react'
-
+import React, { useEffect, useState } from 'react'
 import Example1 from './components/example1'
 import EmbedSDK from './components/EmbedSDK'
-import SelectExample from './components/SelectExample'
-
 import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Link
 } from "react-router-dom";
+import styled from "styled-components"
+
+const params = {
+  examples: [
+    {    
+        url: 'examples/google-login',
+        user: {
+            private: 'asldhfkshjdafl',
+            public: 'lakshdsl',
+        },
+        text: 'Embed with Google Login'
+    },
+    {
+        url: 'examples/google-login-sdk',
+        user: {
+            private: 'asldhfkshjdafl',
+            public: 'lakshdsl',
+        },
+        text: 'SDK with Google Login'
+    },
+    {
+      url: 'examples/embed-sdk',
+      user: {
+          private: 'asldhfkshjdafl',
+          public: 'lakshdsl',
+      },
+      text: 'Basic Embed with SDK'
+    }
+  ]
+} 
+
+
+const SelectExample = () => {
+  // const [heading, setHeading] = 
+  return(
+    <>
+      <h1>Select an Example</h1>
+      <ul>
+      { params.examples.map((example, idx) => {
+        return(
+          <li key={idx}>
+            <Link to={example.url}>{example.text}</Link>
+          </li>
+        )
+      })}
+      </ul>
+    </>
+  )
+
+}
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/examples/google-login'>
-          <Example1 />
-        </Route>
+        <Route path='/' component={SelectExample} />
+        <Route path='/examples/google-login' component={Example1} />
         <Route path='/examples/embed-sdk' component={EmbedSDK} />
-        <Route path='/'>
-          <SelectExample />
-        </Route>
       </Switch>
     </Router>
   )
